0182869241d7648690de7ef21bbb411f6f46ee31
noman1303 <belimnoman1303@gmail.com>
2025-12-12 18:57:21 +0530

    PublicSpecif

--- Sources/LinkDetectore/LinkDetector.swift
+++ Sources/LinkDetectore/LinkDetector.swift
@@ -1,16 +1,13 @@
-import Foundation
-import UIKit
 import SwiftUI
- 
-public struct DetectedItem: Identifiable {
-    public let id = UUID()
-    public let text: String
-    public let url: URL?
-}
+import UIKit
 
-
-public  struct LiveDetectingTextView: UIViewRepresentable {
-    @Binding var text: String
+public struct LiveDetectingTextView: UIViewRepresentable {
+    
+    @Binding public var text: String
+    
+    public init(text: Binding<String>) {
+        self._text = text
+    }
 
     public func makeUIView(context: Context) -> UITextView {
         let tv = UITextView()
@@ -20,42 +17,40 @@ public  struct LiveDetectingTextView: UIViewRepresentable {
         tv.delegate = context.coordinator
         tv.font = UIFont.systemFont(ofSize: 17)
         tv.text = text
-        
         return tv
     }
-
+    
     public func updateUIView(_ uiView: UITextView, context: Context) {
         if uiView.text != text {
             uiView.text = text
         }
     }
-
+    
     public func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
+    
     public class Coordinator: NSObject, UITextViewDelegate {
-        var parent: LiveDetectingTextView
-
-        init(_ parent: LiveDetectingTextView) {
+        
+        public var parent: LiveDetectingTextView
+        
+        public init(_ parent: LiveDetectingTextView) {
             self.parent = parent
         }
-
+        
         public func textViewDidChange(_ textView: UITextView) {
             parent.text = textView.text
             
             let cursor = textView.selectedRange
-
             let attributed = NSMutableAttributedString(string: parent.text)
             let full = NSRange(location: 0, length: attributed.length)
-
-            // REGEX PATTERNS
+            
             let patterns = [
                 "(?i)[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}",         // email
                 "(https?://)?([A-Za-z0-9.-]+\\.[A-Za-z]{2,})[/A-Za-z0-9._%+-]*", // website
                 "\\b[0-9]{7,15}\\b"                                   // phone number
             ]
-
+            
             for pattern in patterns {
                 let regex = try! NSRegularExpression(pattern: pattern, options: .caseInsensitive)
                 let matches = regex.matches(in: parent.text, options: [], range: full)
@@ -76,8 +71,7 @@ public  struct LiveDetectingTextView: UIViewRepresentable {
             textView.selectedRange = cursor
         }
         
-        /// Convert email/phone/url into real tappable URLs
-        func detectURL(for string: String) -> URL? {
+        public func detectURL(for string: String) -> URL? {
             if string.contains("@") { return URL(string: "mailto:\(string)") }
             if Int(string) != nil { return URL(string: "tel:\(string)") }
             if string.hasPrefix("http") { return URL(string: string) }
